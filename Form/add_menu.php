7f6223fa01cc769934dd1331bb43245140345184
VaishnaviSSwami <vaishsswami@gmail.com>
2022-01-25 16:00:32 +0530

    sidebar complete

--- Form/add_menu.php
+++ Form/add_menu.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 
-<title> BELLI RESTRAUNT dashboard</title>
+<title> BELLI RESTRAUNT ADD MENU</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"> 
 <link rel="stylesheet" href="style.css">
@@ -13,10 +13,10 @@
 <body>
 <div class="sidebar">
     <img src="hfhlogo.PNG" height="50" width="50"/><br><br><br><br><br>
-    <a class="active" href="dash_board.html">DASHBOARD</a><br>
-    <a href="add_menu.php">ADD MENU</a><br>
-    <a href="view_menu.php">VIEW MENU</a><br>
-    <a href="myprofile.php">MY PROFILE</a><br><br><br><br><br><br><br><br><br><br><br>
+    <a  href="dash_board.html">DASHBOARD</a>
+    <a class="active" href="add_menu.php">ADD MENU</a>
+    <a href="view_menu.php">VIEW MENU</a>
+    <a  href="myprofile.php">MY PROFILE</a><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <div class="bottom_icon">
         &nbsp;
         <img src="images/setting.PNG" height="40" width="40"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
